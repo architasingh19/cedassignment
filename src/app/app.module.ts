393cf14452d1f619197a82657f79ed5892b964d9
archita-singh19 <archita.singh@gaiansolutions.com>
2024-11-10 12:21:41 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { MainContentComponent } from './Components/main-content/main-content.component';
+import { WealthCalendarComponent } from './Components/wealth-calendar/wealth-calendar.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { WealthContainerComponent } from './Components/wealth-container/wealth-container.component';
+import { FooterComponent } from './Components/footer/footer.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    MainContentComponent,
+    WealthCalendarComponent,
+    HeaderComponent,
+    WealthContainerComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
